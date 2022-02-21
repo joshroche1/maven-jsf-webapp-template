9dac5fabc7660f1f3c1d37ab0bc5671adf7286c1
joshroche1 <joshua.a.roche@gmail.com>
2022-02-21 18:32:40 +0100

    Update DAOlogin.java

--- src/main/java/net/jar/Data/DAOlogin.java
+++ src/main/java/net/jar/Data/DAOlogin.java
@@ -3,7 +3,7 @@ package net.java.Data;
 
 public class DAOlogin {
 
-  public static boolean validatePatient(String user, String pass) {
+  public static boolean validateUser(String user, String pass) {
     String username = user.trim();
     String password = pass.trim();
     if (username.equals("patient")) {
@@ -12,17 +12,6 @@ public class DAOlogin {
       if (password.equals("cmsc495")) { return true; }
     } else if (username.equals("default")) {
       if (password.equals("user")) { return true; }
-    }
-    return false;
-  }
-  public static boolean validateProvider(String user, String pass, String tkn) {
-    String username = user.trim();
-    String password = pass.trim();
-    String token = tkn.trim();
-    if (username.equals("provider")) {
-      if (password.equals("password") && token.equals("1111111")) { return true; }
-    } else if (username.equals("instructor")) {
-      if (password.equals("cmsc495") && token.equals("9999999")) { return true; }
     }
     return false;
   }
